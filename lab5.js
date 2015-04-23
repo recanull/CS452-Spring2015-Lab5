7864c67237f3971fb861c01388ea3b5508bd07d4
recanull <canullre@clarkson.edu>
2015-04-23 19:54:03 -0400

    Update lab5.js

--- lab5.js
+++ lab5.js
@@ -1,24 +1,222 @@
 var gl;
-var points;
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 20.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+
+
+var nRows = 50;
+var nColumns = 50;
+var data = [];
+for( var i = 0; i < nRows; ++i ) {
+    data.push( [] );
+    var x = Math.PI*(4*i/nRows-2.0);
+    
+    for( var j = 0; j < nColumns; ++j ) {
+        var y = Math.PI*(4*j/nRows-2.0);
+        var r = Math.sqrt(x*x+y*y);
+        
+        data[i][j] = r ? Math.sin(r) / r : 1.0;
+    }
+}
+
+var pointsArray = [];
+
+var fColor;
+
+var near = -10;
+var far = 10;
+var radius = 6.0;
+var theta  = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+const black = vec4(0.0, 0.0, 0.0, 1.0);
+const red = vec4(1.0, 0.0, 0.0, 1.0);
+
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+
+var left = -2.0;
+var right = 2.0;
+var ytop = 2.0;
+var bottom = -2.0;
+
+var modeViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var vertexColors = [
+  vec4( 1.0, 0.0, 0.0, 1.0 ),  // black
+	vec4( 1.0, 1.0, 0.0, 1.0 ),  // red
+	vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+	vec4( 0.0, 1.0, 1.0, 1.0 ),  // green
+	vec4( 1.0, 0.0, 1.0, 1.0 ),  // blue
+	vec4( 1.0, 1.0, 1.0, 1.0 ),  // magenta
+	vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+	vec4( 1.0, 1.0, 1.0, 1.0 )   // cyan
+];
+
+var vertices = [
+	vec3( -0.5, -0.5,  0.5 ),
+	vec3( -0.5,  0.5,  0.5 ),
+	vec3(  0.5,  0.5,  0.5 ),
+	vec3(  0.5, -0.5,  0.5 ),
+	vec3( -0.5, -0.5, -0.5 ),
+	vec3( -0.5,  0.5, -0.5 ),
+	vec3(  0.5,  0.5, -0.5 ),
+	vec3(  0.5, -0.5, -0.5 )
+];
+
+
+var numVertices  = 36;
+var indices = [
+	1, 0, 3,
+	3, 2, 1,
+	2, 3, 7,
+	7, 6, 2,
+	3, 0, 4,
+	4, 7, 3,
+	6, 5, 1,
+	1, 2, 6,
+	4, 5, 6,
+	6, 7, 4,
+	5, 4, 0,
+	0, 1, 5
+];
+
 window.onload = function init()
 {
-  var canvas = document.getElementById( "gl-canvas" );
-  gl = WebGLUtils.setupWebGL( canvas );
-  if ( !gl ) { alert( "WebGL isn't available" ); }
-  //
-  // Configure WebGL
-  //
-  gl.viewport( 0, 0, canvas.width, canvas.height );
-  gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-  // Load shaders and initialize attribute buffers
-  var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-  gl.useProgram( program );
-  // Load the data into the GPU
-  // Associate our shader variables with our data buffer
-  render();
-};
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-function render() {
-  gl.clear( gl.COLOR_BUFFER_BIT );
-  // etc.
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+	
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.POLYGON_OFFSET_FILL);
+    gl.polygonOffset(1.0, 2.0);
+
+    for(var i=0; i<nRows-1; i++) {
+        for(var j=0; j<nColumns-1;j++) {
+            pointsArray.push( vec4(2*i/nRows-1, data[i][j], 2*j/nColumns-1, 1.0));
+            pointsArray.push( vec4(2*(i+1)/nRows-1, data[i+1][j], 2*j/nColumns-1, 1.0)); 
+            pointsArray.push( vec4(2*(i+1)/nRows-1, data[i+1][j+1], 2*(j+1)/nColumns-1, 1.0));
+            pointsArray.push( vec4(2*i/nRows-1, data[i][j+1], 2*(j+1)/nColumns-1, 1.0) );
+    }
+}
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+ 
+ 		// Cube
+		var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+    
+    // color cube array atrribute buffer
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+  	gl.enableVertexAttribArray( vColor );
+
+    // vertex cube array attribute buffer
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+		
+    ambientProduct = mult(lightAmbient, materialAmbient);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    specularProduct = mult(lightSpecular, materialSpecular);
+
+    var vBufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    
+    var vPosition2 = gl.getAttribLocation( program, "vPosition2" );
+    gl.vertexAttribPointer( vPosition2, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition2 );
+    
+    fColor = gl.getUniformLocation(program, "fColor");
+ 
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    window.onkeydown = function( event ) {
+      switch( event.keyCode ) {
+        case 37:
+          theta -= dr;
+          break;
+        case 38:
+          phi += dr;
+          break;
+        case 39:
+          theta += dr;
+          break;
+        case 40:
+          phi -= dr;
+          break;
+      } 
+    }
+    
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "ambientProduct"),flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "diffuseProduct"),flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "specularProduct"),flatten(specularProduct) );	
+    gl.uniform4fv( gl.getUniformLocation(program, 
+       "lightPosition"),flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program, 
+       "shininess"),materialShininess );
+		
+    render();
+}
+
+function render(){
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+    var eye = vec3( radius*Math.sin(theta)*Math.cos(phi), 
+                    radius*Math.sin(theta)*Math.sin(phi),
+                    radius*Math.cos(theta));
+    
+    modelViewMatrix = lookAt( eye, at, up );
+    projectionMatrix = ortho( left, right, bottom, ytop, near, far );
+    
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+    // draw each quad as two filled red triangles
+    // and then as two black line loops
+    
+    for(var i=0; i<pointsArray.length; i+=4) { 
+        gl.uniform4fv(fColor, flatten(red));
+        gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+        gl.uniform4fv(fColor, flatten(black));
+        gl.drawArrays( gl.LINE_LOOP, i, 4 );
+    }
+    
+		gl.drawElements( gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0 );
+    requestAnimFrame(render);
 }
